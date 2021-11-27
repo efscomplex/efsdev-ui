1adc055f6239e96ea581f344966497fc7040652e
Edgar <35841638+efscomplex@users.noreply.github.com>
2021-11-27 23:57:37 +0100

    added react router dom

--- examples/app-1/src/App.tsx
+++ examples/app-1/src/App.tsx
@@ -5,6 +5,8 @@ interface AppProps {}
 const { SNOWPACK_PUBLIC_GREET } = __SNOWPACK_ENV__
 
 function App({}: AppProps) {
+  console.log('hello')
+
   return <div className="App">{SNOWPACK_PUBLIC_GREET}</div>
 }
 
